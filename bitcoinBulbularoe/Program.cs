fe5bf25f2d13a941d1d8ced20debabbc956edc98
Opilane TTHK <opilane@tthk.ee>
2022-01-25 13:29:16 +0200

    Add project files.

---
+++ bitcoinBulbularoe/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace bitcoinBulbularoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bitcoiRate bitcoin = GetRates();
+            Console.WriteLine("How much bitcoin you have?");
+            string bitCount = Console.ReadLine();
+            int.Parse(bitCount);
+            Console.WriteLine($"Current rate in {bitcoin.bpi.USD.code}: {bitcoin.bpi.USD.rate_float}");
+            Console.WriteLine($"Current rate in {bitcoin.bpi.EUR.code}: {bitcoin.bpi.EUR.rate_float}");
+            Console.WriteLine($"Current rate in {bitcoin.bpi.GBP.code}: {bitcoin.bpi.GBP.rate_float}");
+        }
+
+        public static bitcoiRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            bitcoiRate bitcoin;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<bitcoiRate>(response);
+            }
+            return bitcoin;
+        }
+    }
+}
